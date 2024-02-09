4488896343495a634595e849cd249d081193b1e5
Sooraj RS <sooraj.soorajrs@gmail.com>
2024-02-10 00:10:53 +0530

    Form Design updates 20240210

--- src/app/components/form-design/form-design.component.ts
+++ src/app/components/form-design/form-design.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-form-design',
@@ -15,9 +15,80 @@ export class FormDesignComponent implements OnInit {
     phone:"4515-333-333"
   };
 
+  progressItems: { name: string, isActive: boolean, isSelected: boolean }[] = [
+    { name: "Added", isActive: false, isSelected: false },
+    { name: "Submitted", isActive: false, isSelected: false },
+    { name: "Verified", isActive: false, isSelected: false },
+    { name: "Approved", isActive: false, isSelected: false },
+    { name: "Completed", isActive: false, isSelected: false }
+  ];
+
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+  @ViewChild('content') content!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getClass(data: any) {
+    if (data.isSelected == true && data.isActive == true) {
+      return 'completed current';
+    }
+    else if (data.isSelected == true && data.isActive == false) {
+      return 'completed';
+    }
+    else if (data.isSelected == false && data.isActive == true) {
+      return 'current';
+    }
+    else {
+      return '';
+    }
+  }
+
+  onClickArrow(data: any) {
+    let index = this.progressItems.findIndex(x => x.name ===data.name);
+    this.progressItems = this.progressItems.map((x,i) => {
+      x.isSelected=false;
+      if(i<index){
+        x.isActive=true;
+      }
+      else  if(i==index){
+        x.isActive=true;
+        x.isSelected=true;
+      }
+      else{
+        x.isActive=false;
+      }
+      return x;
+    })
+  }
+
+  isButtonActive(){
+    let currentItem = this.progressItems.find(x=>x.isActive==true);
+    return currentItem?true:false;
+  }
+
+  saveChange() {
+    this.progressItems = this.progressItems.map((x, i) => {
+      x.isSelected = false;
+      return x;
+    })
+    console.log(this.progressItems)
+  }
+
+
+  scrollRight(): void {
+    let right = document.getElementById('progressarrowcontent');
+    if(right){
+      right.scrollLeft += 100;
+    }
+  }
+
+  scrollLeft(): void {
+    let left = document.getElementById('progressarrowcontent');
+    if(left){
+      left.scrollLeft -= 100;
+    }
+  }
 }
