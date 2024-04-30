197a47a0c1b6b901a1e065b3448473492e2d9da7
Sooraj RS <sooraj.soorajrs@gmail.com>
2024-05-01 00:44:52 +0530

    Added useHash true in router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
